49e5d55987316c81d70ae5178518044927cf5a05
George Lu <georgelu@3542-pop-os.ip>
2023-09-26 23:29:38 +1300

    Revert "changed name to link"
    This reverts commit be8e08ca8d523d5739de5043249ceab398c3a2d0.

--- function_app.py
+++ function_app.py
@@ -5,44 +5,25 @@ app = func.FunctionApp()
 
 # Learn more at aka.ms/pythonprogrammingmodel
 
-def website_clip_recover():
-    tracker_url = input("Enter twitchtracker/streamscharts/sullygnome url:  ")
-    if "streamscharts" in tracker_url:
-        streamer = tracker_url.split("channels/", 1)[1].split("/")[0]
-        vod_id = tracker_url.split("streams/", 1)[1]
-        clip_recover(streamer, vod_id, int(parse_duration_streamscharts(tracker_url)))
-    elif "twitchtracker" in tracker_url:
-        streamer = tracker_url.split("com/", 1)[1].split("/")[0]
-        vod_id = tracker_url.split("streams/", 1)[1]
-        clip_recover(streamer, vod_id, int(parse_duration_twitchtracker(tracker_url)))
-    elif "sullygnome" in tracker_url:
-        streamer = tracker_url.split("channel/", 1)[1].split("/")[0]
-        vod_id = tracker_url.split("stream/", 1)[1]
-        clip_recover(streamer, vod_id, int(parse_duration_sullygnome(tracker_url)))
-    else:
-        print("Link not supported.. Returning to main menu.")
-        return
-
 # Get started by running the following code to create a function using a HTTP trigger.
 
-@app.function_link(link="HttpTrigger1")
+@app.function_name(name="HttpTrigger1")
 @app.route(route="hello")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
       logging.info('Python HTTP trigger function processed a request.')
-      link = req.params.get('link')
-      if not link:
+      name = req.params.get('name')
+      if not name:
          try:
              req_body = req.get_json()
          except ValueError:
              pass
          else:
-             link = req_body.get('link')
+             name = req_body.get('name')
 
-      if link:
-      	 # call function
-         return func.HttpResponse(f"Link is, {link}. This HTTP triggered function executed successfully.")
+      if name:
+         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
       else:
          return func.HttpResponse(
-              "This HTTP triggered function executed successfully. Pass a link in the query string or in the request body fto search for clip.",
+              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
               status_code=200
          )
